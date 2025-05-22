2b2f4f45b39125a2e7b81a7b5abe325e94fb8285
AidaDaiyrbekova <hgirl0108@gmail.com>
2025-05-22 20:45:52 +0600

    'lesson5'

---
+++ lessson5/app.js
@@ -0,0 +1,72 @@
+console.log('hello');
+
+let customer = 'John';
+let customerName = 'Baryt';
+let customer_name = 'Kuban';
+let customerSurname = 'Sariev';
+let $num ='12';
+let $ ='12';
+let _num = 12;
+let _num123 = 123;
+// let var = 123;
+
+// let age = 18
+// console.log('Имя=',customer,' ', 'Фамилия=', customerSurname, 'Возраст=', age)
+// console.log(`Имя=${customer} Фамилия=${customerSurname} Возраст=${age} `)
+// // console.log('OAO "Северэлектро"')
+// console.log("OAO \"Северэлектро\"")
+// let file='d:\\data\\profile.txt'
+// console.log(file)
+// let number=123
+// let number1=3.14
+// console.log(number)
+// number=500
+// console.log(number)
+// const number2=123
+// // number2=500
+
+let a=10
+let b=4
+
+// console.log('a=',a, 'b=',b)
+// console.log(a + b, '+')
+// console.log(a - b, '-')
+// console.log(a / b, '/')
+// console.log(a * b, '*')
+// console.log(a % b, '%')
+// console.log(5 % 2, '5%')
+// console.log(4 % 2, '4%')
+// console.log(a ** b, '**')
+// console.log(a / 0 + 10000000000)
+// console.log(0 / a)
+// console.log(6644864 + 'asd%')
+// console.log(6644864 + 210)
+// console.log(300 * '2')
+
+//  a=10
+//  b=5
+// console.log(a>b, 'a>b')
+// console.log(a<b, 'a<b')
+// console.log(a>=b, 'a>=b')
+
+let age1 = prompt('сколько тебе лет')
+console.log(age1, 'age')
+console.log(typeof age1, 'typeof age1')
+
+// if (true) {
+//  console.log('true')
+// } else {
+//  console.log('else')
+// }
+
+if (age1>1000) {
+ console.log('вы бесмертный')
+} else if (age1>18){
+ console.log('Вы соверш')
+} else if (age1<0){
+ console.log('Вы  не соверш')
+}
+else {
+ console.log('вы непр указ возр')
+}
+
